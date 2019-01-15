55d2ce5b21df81f5d76d984c5230a87afadec90e
Guo Haoming <fsguohaoming@hotmail.com>
2019-01-15 14:11:05 +0800

    Request controller

---
+++ InventoryWeb/Controllers/ManageRequestController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InventoryWeb.Controllers
+{
+    public class ManageRequestController : Controller
+    {
+        public ActionResult RaiseRequest()
+        {
+            return View();
+        }
+    }
+}

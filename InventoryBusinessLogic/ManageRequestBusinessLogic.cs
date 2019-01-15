0d48ea681431cc93b0faa5fd02681ce93fd853c1
PadmapriyaNarayanasamy <padmapriya.n026@gmail.com>
2019-01-15 13:57:13 +0800

    ManageRequestBusinessLogic

---
+++ InventoryBusinessLogic/ManageRequestBusinessLogic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InventoryBusinessLogic.Entity;
+namespace InventoryBusinessLogic
+{
+    class ManageRequestBusinessLogic
+    {
+        Inventory inventory = new Inventory();
+
+        public void ApproveOrRejectRequest(string Status)
+        {
+
+        }
+    }
+}

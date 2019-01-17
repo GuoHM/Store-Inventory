aeb42cfc3e70084b1a58ac20209d6636063af126
Ronith900 <44728044+Ronith900@users.noreply.github.com>
2019-01-17 15:24:03 +0800

    Delegation of Dep Head and Dep Rep

---
+++ InventoryWeb/Controllers/DepManagerController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using InventoryBusinessLogic;
+
+namespace InventoryWeb.Controllers
+{
+    public class DepManagerController : Controller
+    {
+        UserBusinessLogic BL = new UserBusinessLogic();
+        // GET: DepManager
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public ActionResult AssignDepRep()
+        {
+            ViewBag.depList = BL.getDepUsers();
+            return View();
+        }
+
+        public ActionResult saveNewRep(string dropdown1)
+        {
+            BL.UpdateDepRep(dropdown1);
+            return View("Index");
+        }
+
+        public ActionResult AssignDepHead()
+        {
+            ViewBag.depHead = BL.appointNewDepHead();
+            return View();
+        }
+
+        public ActionResult saveDepHead(string dropdown1, DateTime date1, DateTime date2)
+        {
+            BL.UpdateDepHead(dropdown1, date1, date2);
+            return View("Index");
+        }
+
+
+
+    }
+}

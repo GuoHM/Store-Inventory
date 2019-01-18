13c418d326735232f8b0bd26eddcaf7da66ffd49
Guo Haoming <fsguohaoming@hotmail.com>
2019-01-18 11:31:37 +0800

    purchase order ui

--- InventoryWeb/Scripts/tables/PurchaseOrder.js
+++ InventoryWeb/Scripts/tables/PurchaseOrder.js
@@ -11,7 +11,7 @@ var TableInit = function () {
     oTableInit.Init = function () {
         $('#SearchItemTable').bootstrapTable({
             method: 'get',
-            //url: 'http://inventorywebapi20190116011936.azurewebsites.net/api/Catalogue',
+            url: "https://inventorywebapi2019.azurewebsites.net/api/Catalogue",
             //toolbar: '#toolbar',                //工具按钮用哪个容器
             striped: true, // 是否显示行间隔色
             cache: false, // 是否使用缓存，默认为true，所以一般情况下需要设置一下这个属性（*）
@@ -21,7 +21,7 @@ var TableInit = function () {
             queryParams: oTableInit.queryParams,// 传递参数（*）
             sidePagination: "client", // 分页方式：client客户端分页，server服务端分页（*）
             pageNumber: 1, // 初始化加载第一页，默认第一页
-            pageSize: 10, // 每页的记录行数（*）
+            pageSize: 6, // 每页的记录行数（*）
             pageList: [10, 25, 50, 100], // 可供选择的每页的行数（*）
             search: true, //是否显示表格搜索，此搜索是客户端搜索，不会进服务端，所以，个人感觉意义不大
             strictSearch: false,
@@ -41,61 +41,43 @@ var TableInit = function () {
                 align: "center",
                 title: 'ItemCode',
                 sortable: true,
-                sortable: true,
-                field: 'Description'
+                field: 'ItemID'
             }, {
                 align: "center",
                 title: 'Description',
                 sortable: true,
-                sortable: true,
-                //field : 'ID',
-                //events: operateEvents,
-                //formatter: InputTextBox
+                field: 'Description'
                 }, {
                     align: "center",
                     title: 'Quantity',
                     sortable: true,
                     sortable: true,
-                //field : 'ID',
-                //events: operateEvents,
-                //formatter: InputTextBox
+                field : 'Quantity',
                 }, {
                 align: "center",
                 title: 'Reorder Quantity',
                 sortable: true,
-                sortable: true,
-                //field : 'ID',
-                //events: operateEvents,
-                //formatter: selectItem
+                field: 'ReorderQuantity'
                 }, {
                     align: "center",
                     title: 'Order Quantity',
                     sortable: true,
-                    sortable: true,
-                    //field : 'ID',
-                    //events: operateEvents,
-                    //formatter: selectItem
+                    formatter: InputTextBox
                 }, {
                     align: "center",
                     title: 'Price',
                     sortable: true,
-                    sortable: true,
+                    field: 'Price'
                 }, {
                     align: "center",
                     title: 'Supplier',
                     sortable: true,
-                    sortable: true,
-                    //field : 'ID',
-                    //events: operateEvents,
-                    //formatter: selectItem
+                    field: 'Supplier1'
                 }, {
                     align: "center",
                     title: 'Select',
                     sortable: true,
-                    sortable: true,
-                    //field : 'ID',
-                    //events: operateEvents,
-                    //formatter: selectItem
+                    formatter: selectItem
                 }
             ],
             formatLoadingMessage: function () {
@@ -133,62 +115,35 @@ var TableInit = function () {
             columns: [{
                 align: "center",
                 title: 'ItemCode',
-                sortable: true,
-                sortable: true,
-                field: 'Description'
+                sortable: true
             }, {
                 align: "center",
                 title: 'Description',
-                sortable: true,
-                sortable: true,
-                //field : 'ID',
-                //events: operateEvents,
-                //formatter: InputTextBox
+                sortable: true
             }, {
                 align: "center",
                 title: 'Quantity',
-                sortable: true,
-                sortable: true,
-                //field : 'ID',
-                //events: operateEvents,
-                //formatter: InputTextBox
+                sortable: true
             }, {
                 align: "center",
                 title: 'Reorder Quantity',
-                sortable: true,
-                sortable: true,
-                //field : 'ID',
-                //events: operateEvents,
-                //formatter: selectItem
+                sortable: true
             }, {
                 align: "center",
                 title: 'Order Quantity',
-                sortable: true,
-                sortable: true,
-                //field : 'ID',
-                //events: operateEvents,
-                //formatter: selectItem
+                sortable: true
             }, {
                 align: "center",
                 title: 'Total Price',
-                sortable: true,
-                sortable: true,
+                sortable: true
             }, {
                 align: "center",
                 title: 'Supplier',
-                sortable: true,
-                sortable: true,
-                //field : 'ID',
-                //events: operateEvents,
-                //formatter: selectItem
+                sortable: true
             }, {
                 align: "center",
                 title: 'Remove',
-                sortable: true,
-                sortable: true,
-                //field : 'ID',
-                //events: operateEvents,
-                //formatter: selectItem
+                sortable: true
             }
             ],
             formatLoadingMessage: function () {
@@ -197,18 +152,9 @@ var TableInit = function () {
         });
     };
 
-
-    // params
-    oTableInit.queryParams = function (params) {
-
-        var temp = {
-            courseid: $("#courseid").val()
-        };
-        return temp;
-    };
     function InputTextBox(value, row, index) {
         return [
-            '<input type="text" maxlength="5" class="form-control" placeholder="CourseID" id="quantity">'
+            '<input type="text" maxlength="5" class="form-control" placeholder="Quantity" id="quantity">'
         ].join('');
     }
     function selectItem(value, row, index) {
@@ -216,28 +162,6 @@ var TableInit = function () {
             '<input type="button" value="Select" onclick="selectItem(this)" class="btn btn-primary" />',
         ].join('');
     }
-
-    operateEvents = {
-        'click .like': function (e, value, row, index) {
-            $("#editEnrollmentModal").modal('show');
-            $("#studentnameEdit").val(row.account.name);
-            $("#useridEdit").val(row.id.userid);
-            $("#coursenameEdit").val(row.course.courseName);
-            $("#courseidEdit").val(row.id.courseid);
-            $("#enrollmentDateEdit").val(row.enrollmentDate);
-            $("#gradesEdit").val(row.grades);
-            var date = document.getElementById("enrollmentDateEdit").value;
-            var grades = document.getElementById("gradesEdit").value;
-            var url = 'editErollment/' + row.id.userid + '/' + date + '/' + grades + '/' + row.id.courseid;
-            $("#editForm").attr('action', url);
-        },
-        'click .remove': function (e, value, row, index) {
-            $("#deleteEnrollmentModal").modal('show');
-            var url = 'deleteErollment/' + row.id.userid + '/' + row.id.courseid;
-            $("#deleteForm").attr('action', url);
-        }
-    };
-
 
     return oTableInit;
 };
@@ -252,17 +176,7 @@ var ButtonInit = function () {
             $("#addEnrollmentModal").modal('show')
         })
 
-
     };
 
     return oInit;
 };
-
-function editFunction() {
-    var stu = document.getElementById("useridEdit").value;
-    var course = document.getElementById("courseidEdit").value;
-    var date = document.getElementById("enrollmentDateEdit").value;
-    var grades = document.getElementById("gradesEdit").value;
-    var url = 'editErollment/' + stu + '/' + date + '/' + grades + '/' + course;
-    $("#editForm").attr('action', url);
-}

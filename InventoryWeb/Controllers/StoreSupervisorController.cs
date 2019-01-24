28c6e2b76c4d0a65b2cb0d45acaaf425b6c331f4
Anjaneyulu Chalavadi <anjaneyulu_ch1981@outlook.com>
2019-01-24 11:46:58 +0800

    adding StoreSupervisor

---
+++ InventoryWeb/Controllers/StoreSupervisorController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using InventoryBusinessLogic.Entity;
+using InventoryBusinessLogic;
+using Microsoft.AspNet.Identity;
+
+using System.Web.Script.Serialization;
+
+
+
+
+
+namespace InventoryWeb.Controllers
+{
+    public class StoreSupervisorController : Controller
+    {
+
+        AdjustmentBusinessLogic req = new AdjustmentBusinessLogic();
+
+        // GET: StoreSupervisor
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ViewInventory()
+        {
+            return View();
+        }
+
+        public ActionResult ViewAdjustmentVoucher()
+        {
+            string userId = User.Identity.GetUserId();
+            ViewBag.userID = userId;
+            new AdjustmentBusinessLogic().getAllAdjustment(User.Identity.Name);
+            return View();
+        }
+
+
+        public ActionResult ViewAdjustmentVoucherbyID()
+        {
+            string userId = User.Identity.GetUserId();
+            new AdjustmentBusinessLogic().getAllAdjustment(userId);
+            return View();
+        }
+        public ActionResult Save(string itemid, int reorderlevel, int reorderquantity, int price)
+        {
+            new CatalogueBusinessLogic().Save(itemid, reorderlevel, reorderquantity, price);
+            return View("ViewInventory");
+        }
+
+        public ActionResult ViewAdjustmentVoucherItems(int adjustmentID)
+        {
+
+            new AdjustmentBusinessLogic().getAllAdjItems(adjustmentID);
+            return View();
+
+        }
+
+      
+        [HttpPost]
+        public ActionResult SaveRequestStatus()
+        {
+            var sr = new System.IO.StreamReader(Request.InputStream);
+            var stream = sr.ReadToEnd();
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            var list = js.Deserialize<List<SelectedList>>(stream);
+
+            if (list.Any())
+            { 
+                    new AdjustmentBusinessLogic().ApproveOrRejectRequest(list[0].AdjustmentID, list[0].requestStatus, list[0].remarks);
+                    
+            }
+            return new JsonResult();
+        }
+
+        class SelectedList
+        {
+            public int AdjustmentID { get; set; }
+
+            public string requestStatus { get; set; }
+            public string remarks { get; set; }
+
+        }
+
+    }
+
+
+}
